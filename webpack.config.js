1d50ef7081779d932286b090b1ae168a5b34a8b5
Jonathan M. Wilbur <jonathan@wilbur.space>
2020-06-17 23:16:36 -0400

    Fix repo files

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
     ],
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: ".min.js",
-        library: "",
+        filename: "cms.min.js",
+        library: "cms",
         libraryTarget: "var",
     },
     // mode: "development",
